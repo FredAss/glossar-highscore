a08583d7e1f049e7e719e38cb66f0b01f0ad67cc
Knut Hühne <knut@k-nut.eu>
2014-06-21 22:22:07 +0200

    use sqlalchemy, python3

---
+++ entry.py
@@ -0,0 +1,26 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/knut/glossar-highscore/test.db'
+db = SQLAlchemy(app)
+
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    score = db.Column(db.Integer)
+    time = db.Column(db.Float)
+    datetime = db.Column(db.DateTime)
+
+    def __init__(self, name, score, time):
+        self.name = name
+        self.time = time
+        self.score = score
+        self.datetime = datetime.now()
+
+    def __repr__(self):
+        return '<Entry %r>' % self.name
+
+
--- main.py
+++ main.py
@@ -1,51 +1,21 @@
 # all the imports
-import sqlite3
-import os
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash, jsonify 
+from flask import Flask, request, abort
 from cross import crossdomain
+import os
 import json
 import datetime
+from entry import db, Entry
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-# Load default config and override config from an environment variable
+# load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'highscores.db'),
-    DEBUG=True
+    #debug=True
     ))
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-
-@app.route("/highscore", methods=['GET', 'POST'])
+@app.route("/highscore", methods=['get', 'post'])
 @crossdomain(origin='*')
 def handle_highscore():
     if request.method == "POST":
@@ -58,19 +28,17 @@ def handle_highscore():
 
 
 def return_top_ten():
-    db = get_db()
-    cur = db.execute("""select name, score, time, datetime from highscore order by
-            score desc, time asc limit 10""")
-    scores = cur.fetchall()
+    scores = Entry.query.order_by(Entry.score.desc(), Entry.time.asc()).limit(10)
     jsonscores = []
-    for row in scores:
-        jsonscores.append({"name": row["name"],
-                           "score": row["score"],
-                           "time": row["time"],
-                           "datetime": row["datetime"]
+    for entry in scores:
+        jsonscores.append({"name": entry.name,
+                           "score": entry.score,
+                           "time": entry.time,
+                           "datetime": str(entry.datetime)
                            })
 
     return json.dumps(jsonscores)
+
 
 def add_score(score, time):
     try:
@@ -78,43 +46,30 @@ def add_score(score, time):
         time = float(time)
     except ValueError:
         return abort(400)
-    parts = [request.form["name"], request.form["score"], request.form["time"], datetime.datetime.now()]
-    db = get_db()
-    values = db.execute('''insert into highscore (name, score, time, datetime) values (?, ?, ?, ?)''', parts)
-    db.commit()
-    newId = values.lastrowid
-    allScores = db.execute('''select id from highscore order by score
-                           desc limit 10''').fetchall()
-    highscore = False
-    for t in allScores:
-        if newId in t:
-            highscore = True
-    return json.dumps({"highscore": highscore})
+    entry = Entry(request.form['name'], request.form['score'], request.form['time'])
+    db.session.add(entry)
+    db.session.commit()
+    return json.dumps({"highscore": True})
 
 
-@app.route("/highscore/check", methods=["POST"])
+@app.route("/highscore/check", methods=["post"])
 @crossdomain(origin="*")
 def check_if_in_top_10():
-    # right now we are only checking for score 
-    # not for the time. This needs fixing
+    score = request.get['score']
     score = int(request.form["score"])
     time = int(request.form["time"])
-    db = get_db()
-    cur = db.execute("""select score, time from highscore order by
-            score desc, time asc limit 10""")
-    scores = cur.fetchall()
+    scores = Entry.query().order_by(Entry.score.desc(), Entry.time.asc()).limit(10)
     if len(scores) < 10:
-        return json.dumps({"top10" : True})
+        return json.dumps({"top10": True})
     highscore = 1000000000000000000
     hightime = 0
-    for row in scores:
-        if row["score"] < highscore:
-            highscore = row["score"]
-            hightime = row['time']
+    for entry in scores:
+        if entry.score < highscore:
+            highscore = entry.score
+            hightime = entry.time
 
     return json.dumps({"top10": (score > highscore or (score == highscore and hightime > time))
                        })
-
 
 
 if __name__ == "__main__":
